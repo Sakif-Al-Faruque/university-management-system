3861cab810e37c38f1a3e8ed6b151a3ec9b4b9e9
MahinMuhammad <amimahinurrahman@gmail.com>
2023-09-01 12:47:28 +0600

    student auth routing

---
+++ app/api/student/auth/signup/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse, NextRequest } from "next/server";
+import Student from "@/model/StudentModel";
+import { Connect } from "@/dbConfig/dbConfig";
+import bcrypt from "bcrypt";
+
+Connect();
+
+export async function POST(request: NextRequest) {
+    try{
+        const {
+            fullname,
+            email,
+            password,
+            phone,
+            address,
+            salary
+        } = await request.json();
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const student = new Student({
+            fullname: fullname,
+            email: email,
+            password: hashedPassword,
+            phone: phone,
+            address: address,
+            salary: salary
+        });
+
+        const savedStudent = await student.save();
+
+        return NextResponse.json({
+            message: "Signup successful...",
+            user: savedStudent,
+            seccess: true
+        },
+        {status: 200}
+        );
+    }catch(error: any){
+        return NextResponse.json({
+            error: error.message
+        },
+        {status: 500}
+        )
+    }    
+}
---
+++ model/StudentModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const StudentSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+    },
+    
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        require: true,
+    },
+
+    phone: {
+        type: String,
+    },
+
+    address: {
+        type: String,
+    },
+
+    salary: {
+        type: Number,
+    },
+
+    approved: {
+        type: Boolean,
+        default: false,
+    },
+
+    usertoken: {
+        type: String,
+        default: ""
+    },
+});
+
+const Student = mongoose.models.students || mongoose.model("students", StudentSchema);
+
+export default Student;
